47a666648cdad71b5672f2f1dfcb4ecd8cf9fd84
Gregory Bober <g.bober@gmail.com>
2024-07-29 14:14:42 +0200

    pageentry fix

--- src/createSchemaCustomization.mjs
+++ src/createSchemaCustomization.mjs
@@ -148,7 +148,7 @@ export default async function createSchemaCustomization({
 			if (field.type === 'URI') hasUriField = true
 		}
 
-		if (hasUriField) {
+		if (hasUriField && !typeDef.interfaces.includes('Redirect')) {
 			typeDef.interfaces.push('PageEntry')
 			typeDef.fields['breadcrumbs'] = {
 				type: '[PageEntry]',
